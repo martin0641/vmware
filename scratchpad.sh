ffa2d8de19b88e504df3aff64f27bac495a1169b
martin0641 <martin0641@gmail.com>
2021-11-08 08:39:46 -0500

    1

--- scratchpad.sh
+++ scratchpad.sh
@@ -6,3 +6,11 @@ f=$monnumber ; echo $monhostname`seq 1 $f`.$fqdn
 printf \n
 printf $monipnumeration
 printf \n
+
+for ((i=$monnumber;i<=1;i++)); do
+        if 
+    echo $monhostname  $monipstart
+    monipstart=$monipstart+1
+done
+
+f=$monnumber ; echo $monhostname`seq 1 $f`.$fqdn
